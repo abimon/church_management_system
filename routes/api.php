2ab89b26850c7a5d63826858079321087e9c2f5c
abimon <eddyabby59@gmail.com>
2025-09-24 15:12:52 +0300

    user and church controller routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
+use App\Http\Controllers\ChurchController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resources([
+    'users' => UserController::class,
+    'church'=>ChurchController::class,
+]);
